1e0700065a17a8a84615e13e016fb787bfa2cb2e
Stephen Shelton <stephen.shelton@gsa.gov>
2022-07-31 16:10:24 -0400

    Seeing if it's a script issue

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxYSXdRhS9UKJYlQojBzDLZ3CLfdc6322B6Bxgb2ksvfciQto8rvq2o34JLTasQvxXvlw/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzY0EPtafya_lSlry65Wn7ukaZ53XgDGC2UZrNNTX5gWFfKtFFu0-ZGTE1t2OqOCYvY7Q/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
